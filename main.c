336d645de236e75b57acb90e9e1d5e9e17b536bd
黑牛 <chenlingmin.com@gmail.com>
2021-04-21 18:56:32 +0800

    Part 1 - Introduction and Setting up the REPL

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+typedef struct {
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_lenth;
+} InputBuffer;
+
+void close_input_buffer(InputBuffer *input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+void read_input(InputBuffer *input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_lenth = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void print_prompt() {
+    printf("db > ");
+}
+
+InputBuffer *new_input_buffer() {
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_lenth = 0;
+    return input_buffer;
+}
+
+int main(int argc, char *argv[]) {
+    InputBuffer *input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
+}
